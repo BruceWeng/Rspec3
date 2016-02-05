2ea41c17552c154edf7089c6edd3d306172a9ca8
BruceWeng <weng8916@gmail.com>
2016-02-04 18:44:16 -0800

    matchers with 9 methods

--- spec/bootstrap_spec.rb
+++ spec/bootstrap_spec.rb
@@ -1,8 +1,8 @@
 require "bootstrap"
 
-describe Bootstrap do #define test case
-  it "says hello" do #actual test
-    #create a new instance called hello(method name) in Bootstrap class expecting include "Hello"
+describe Bootstrap do # define test case
+  it "says hello" do # actual test
+    # create a new instance called hello(method name) in Bootstrap class expecting include "Hello"
     expect(Bootstrap.new.hello).to include "Hello"
   end
 end
